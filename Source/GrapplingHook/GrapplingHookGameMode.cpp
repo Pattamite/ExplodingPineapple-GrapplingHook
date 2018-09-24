0098101060c31230e48e7aae4e4a6bd8740611b8
Pattnatt <jittapat007@gmail.com>
2018-09-24 15:57:44 +0300

    Init Project

---
+++ Source/GrapplingHook/GrapplingHookGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "GrapplingHookGameMode.h"
+#include "GrapplingHookCharacter.h"
+
+AGrapplingHookGameMode::AGrapplingHookGameMode()
+{
+	// Set default pawn class to our character
+	DefaultPawnClass = AGrapplingHookCharacter::StaticClass();	
+}
