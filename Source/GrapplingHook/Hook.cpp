f8e71e232aeae0b0d8d78c592376b79055c1ac3c
pawissanutt <pawissanutt@gmail.com>
2018-10-04 18:19:15 +0700

    Migrate some part of hook system to HookShooter as ActorComponent with c++

--- Source/GrapplingHook/Hook.cpp
+++ Source/GrapplingHook/Hook.cpp
@@ -24,19 +24,9 @@ void AHook::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
+//
+//void AHook::SetCharacter(APawn * character)
+//{
+//}
 
-void AHook::Setup(UStaticMeshComponent* RootSM, UStaticMeshComponent* TargetSM)
-{
-	FConstraintInstance ConstraintInstance;
-	UPhysicsConstraintComponent* ConstraintComp = NewObject<UPhysicsConstraintComponent>(RootSM);
-	if (!ConstraintComp)
-	{
-		//UE_LOG constraint UObject could not be created!
-		return;
-	}
-
-	ConstraintComp->SetWorldLocation(GetActorLocation());
-	ConstraintComp->AttachTo(GetRootComponent(), NAME_None, EAttachLocation::KeepWorldPosition);
-	ConstraintComp->SetConstrainedComponents(RootSM, NAME_None, TargetSM, NAME_None);
-}
 
--- Source/GrapplingHook/HookShooter.cpp
+++ Source/GrapplingHook/HookShooter.cpp
@@ -10,7 +10,6 @@ UHookShooter::UHookShooter()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
 }
 
 
@@ -30,5 +29,81 @@ void UHookShooter::TickComponent(float DeltaTime, ELevelTick TickType, FActorCom
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+bool UHookShooter::IsOnHook()
+{
+	return onHook;
+}
+
+AHook* UHookShooter::GetHook()
+{
+	return hook;
+}
+
+AHook* UHookShooter::ShootHookByDirection(FVector direction, float hookLength)
+{
+	direction.Normalize();
+	FVector start = GetOwner()->GetActorLocation();
+	FVector end = start + direction * hookLength;
+
+	//UE_LOG(LogTemp, Warning, TEXT("s: %s e: %s"), *start.ToString(), *end.ToString());
+
+	FHitResult HitResult;
+	FCollisionQueryParams TraceParameters(FName(TEXT("")), false, GetOwner());
+	UWorld* const World = GetWorld();
+	if (!World) {
+		UE_LOG(LogTemp, Warning, TEXT("world is null"));
+		return nullptr;
+	}
+	bool hit = World->LineTraceSingleByObjectType(
+		OUT HitResult,
+		start,
+		end,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_WorldStatic),
+		TraceParameters
+	);
+	if (hit) {
+		UE_LOG(LogTemp, Warning, TEXT("Hit at %s"), *HitResult.Location.ToString());
+		return SetHook(HitResult.Location);
+	}
+	return nullptr;
+}
+
+AHook* UHookShooter::SetHook(FVector location)
+{
+	UWorld* const World = GetWorld();
+	if (World) {
+		FActorSpawnParameters SpawnParams;
+		//SpawnParams.Instigator = this;
+		FRotator rotator = FRotator::ZeroRotator;
+		AHook* newHook = World->SpawnActor<AHook>(HookBP, location, rotator, SpawnParams);
+		if (newHook) {
+			UE_LOG(LogTemp, Warning, TEXT("Create Hook at %s"), *location.ToString());
+			hook = newHook;
+			onHook = true;
+			return hook;
+		}
+		else
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Can not create hook"));
+		}
+	}
+	return nullptr;
+}
+
+void UHookShooter::OnHook()
+{
+}
+
+void UHookShooter::RemoveHook()
+{
+	UWorld* const World = GetWorld();
+	if (World && hook) {
+		World->DestroyActor(hook);
+		onHook = false;
+		hook = nullptr;
+	}
+
 }
 
