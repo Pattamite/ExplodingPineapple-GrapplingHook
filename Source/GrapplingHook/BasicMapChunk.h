ccb84296d0850fe6ea2197a566c7a183a5a31b69
Pattnatt <jittapat007@gmail.com>
2018-11-05 21:26:30 +0300

    Create BasicMapChunk

---
+++ Source/GrapplingHook/BasicMapChunk.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Runtime/Engine/Classes/Engine/TargetPoint.h"
+#include "BasicMapChunk.generated.h"
+
+UCLASS()
+class GRAPPLINGHOOK_API ABasicMapChunk : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABasicMapChunk();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+    UFUNCTION(BlueprintCallable, Category = "Map Position")
+        const FVector GetSpawnLocation(FVector lastExitPoint);
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        FVector entryPoint;
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        FVector exitPoint;
+
+    
+
+};
