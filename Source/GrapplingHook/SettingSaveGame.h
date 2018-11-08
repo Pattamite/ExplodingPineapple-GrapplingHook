a05309b3e9faa8d3e76031ba8ef3b3aa6b931dd9
Pattnatt <jittapat007@gmail.com>
2018-11-08 14:28:59 +0300

    Create GameSettingSystem & SettingSaveGame class

---
+++ Source/GrapplingHook/SettingSaveGame.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SaveGame.h"
+#include "SettingSaveGame.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GRAPPLINGHOOK_API USettingSaveGame : public USaveGame
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
